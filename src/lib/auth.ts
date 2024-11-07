044d34673076011aea3238b489b7a42a290643fe
Bereket Engida <bekacru@gmail.com>
2024-11-07 21:21:18 +0300

    chore: upgrade

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,7 +1,6 @@
 import { betterAuth } from "better-auth";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
 import { db } from "./db";
-import { multiSession } from "better-auth/plugins";
 
 export const auth = betterAuth({
     database: mongodbAdapter(db),
