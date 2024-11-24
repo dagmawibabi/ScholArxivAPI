85675c0dfc7da5294ba632acc6cf0e2835bc6423
dagmawibabi <1babidagi@gmail.com>
2024-11-24 04:57:04 +0300

    honocor

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { serve } from "@hono/node-server";
 import { Hono } from "hono";
-// import { cors } from "hono/cors";
-var cors = require("cors");
+import { cors } from "hono/cors";
+// var cors = require("cors");
 import { auth } from "./lib/auth";
 import arxiv from "./routes/arxiv";
 import gemini from "./routes/ai";
